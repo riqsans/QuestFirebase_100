4220d3a0c1e8b4ebd0705d16566175be2cdf88f1
riqsans <thrq88@gmail.com>
2026-01-02 20:57:06 +0700

    menambahkan objek DestinasiHome untuk definisi rute navigasi beranda

---
+++ app/src/main/java/com/example/questfirebase_100/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_100.view.route
+
+import com.example.questfirebase_100.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
