c9128bfa736431ace54b0340f119d42d97a4213e
riqsans <thrq88@gmail.com>
2026-01-02 20:59:54 +0700

    menambahkan fungsi DataSiswaApp dan HostNavigasi untuk mengatur navigasi antar halaman (Home dan Entry)

---
+++ app/src/main/java/com/example/questfirebase_100/view/controlNavigasi/PetaNavigasi.kt
@@ -0,0 +1,49 @@
+package com.example.questfirebase_100.view.controlNavigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questfirebase_100.view.EntrySiswaScreen
+import com.example.questfirebase_100.view.HomeScreen
+import com.example.questfirebase_100.view.route.DestinasiDetail
+import com.example.questfirebase_100.view.route.DestinasiEntry
+import com.example.questfirebase_100.view.route.DestinasiHome
+
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    // Navigasi ke detail dengan membawa argumen ID
+                    navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
+}
