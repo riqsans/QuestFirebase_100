aebcdc58f99c1a63d637ec93a46f0f185b7798fc
riqsans <thrq88@gmail.com>
2026-01-02 19:57:38 +0700

    menambahkan ContainerApp dan implementasi dependency injection manual untuk RepositorySiswa

---
+++ app/src/main/java/com/example/questfirebase_100/repositori/ContainerApp.kt
@@ -0,0 +1,23 @@
+package com.example.questfirebase_100.repositori
+
+
+import android.app.Application
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        // Pastikan class FirebaseRepositorySiswa sudah kamu buat sebelumnya
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
