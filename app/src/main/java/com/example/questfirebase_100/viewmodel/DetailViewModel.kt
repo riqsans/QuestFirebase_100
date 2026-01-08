98f8e9a4ff6586c069cb24aac66f3599ef96fdbd
riqsans <thrq88@gmail.com>
2026-01-08 11:10:41 +0700

    menambahkan implementasi DetailViewModel dan sealed interface StatusUIDetail

--- app/src/main/java/com/example/questfirebase_100/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_100/viewmodel/DetailViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.questfirebase_100.viewmodel
 
-class DetailViewModel {
+import com.example.questfirebase_100.modeldata.Siswa
+import com.example.questfirebase_100.repositori.RepositorySiswa
+import com.example.questfirebase_100.view.route.DestinasiDetail
+
+@file:OptIn(InternalSerializationApi::class)
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
+
