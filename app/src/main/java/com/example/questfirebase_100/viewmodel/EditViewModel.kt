3337251e54714dbe0c8f3c283f9a353e55b61308
riqsans <thrq88@gmail.com>
2026-01-08 11:17:22 +0700

    menambahkan fungsi editSatuSiswa pada EditViewModel untuk memperbarui data siswa

--- app/src/main/java/com/example/questfirebase_100/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_100/viewmodel/EditViewModel.kt
@@ -39,5 +39,14 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
         }
     }
 
-   
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
 }
