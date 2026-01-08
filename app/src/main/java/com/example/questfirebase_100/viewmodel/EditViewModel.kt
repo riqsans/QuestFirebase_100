ecd5e8b2060aea583f334879615b375d87b6cbf6
riqsans <thrq88@gmail.com>
2026-01-08 11:16:54 +0700

    menambahkan EditViewModel untuk mengelola state dan logika edit data siswa

--- app/src/main/java/com/example/questfirebase_100/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_100/viewmodel/EditViewModel.kt
@@ -1,2 +1,35 @@
 package com.example.questfirebase_100.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_100.modeldata.DetailSiswa
+import com.example.questfirebase_100.modeldata.UIStateSiswa
+import com.example.questfirebase_100.modeldata.toDataSiswa
+import com.example.questfirebase_100.repositori.RepositorySiswa
+import com.example.questfirebase_100.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUIStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
