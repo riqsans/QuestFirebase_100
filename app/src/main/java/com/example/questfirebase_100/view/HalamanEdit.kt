26a2327038b96cdefe97127316cb590b8d47d9dd
riqsans <thrq88@gmail.com>
2026-01-08 11:41:18 +0700

    menambahkan fungsi composable EditSiswaScreen pada HalamanEdit

--- app/src/main/java/com/example/questfirebase_100/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questfirebase_100/view/HalamanEdit.kt
@@ -1,2 +1,10 @@
 package com.example.questfirebase_100.view
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
